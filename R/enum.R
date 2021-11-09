996282248545b72cbac50b47bce03b24ec981c2a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-11-09 13:44:58 +0100

    try something

---
+++ R/enum.R
@@ -0,0 +1,18 @@
+#' Create an enumeration to be used instead of arbitrary values in code.
+#'
+#' @param enumValues Values to use for the enum
+#'
+#' @return the Enum created
+#' @examples
+#' # Without predefined values
+#' Color <- enum(c("Red", "Blue", "Green"))
+#' Color
+#' @export
+enum <- function(enumValues) {
+  myEnum <- as.list(enumValues)
+  enumNames <- names(myEnum)
+
+  if (is.null(enumNames)) names(myEnum) <- myEnum
+
+  return(myEnum)
+}
