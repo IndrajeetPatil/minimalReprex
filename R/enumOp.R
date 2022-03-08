f0b2a2537edac6f0f0f5365ad5432d5fe57730cc
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-08 16:56:18 +0100

    export named list from given package

---
+++ R/enumOp.R
@@ -0,0 +1,11 @@
+#' Enumerated list of operations and their symbols
+#' @export
+enumOp <- function() {
+  list(
+    "add"      = "+",
+    "subtract" = "-",
+    "multiply" = "*",
+    "divide"   = "/"
+  )
+}
+
