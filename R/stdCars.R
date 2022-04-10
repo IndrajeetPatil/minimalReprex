a23dccd7ee579994fe9cd3081b77c7b6840ee7e5
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-10 19:33:31 +0200

    clean up

--- R/stdCars.R
+++ R/stdCars.R
@@ -1,8 +1,8 @@
-#' @title Plot cars data
+#' @title Standard deviations for cars data
 #' @examples
 #' if (require("stats")) {
 #'  sd(cars$speed)
 #'  sd(cars$dist)
 #' }
 #' @export
-plotter <- function() {}
+stdCars <- function() NULL
