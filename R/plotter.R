de04d1f797a91c8c207c8bbbf1b6e1becd6c00bf
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-10 14:46:23 +0200

    also include ggplot plots

--- R/plotter.R
+++ R/plotter.R
@@ -7,4 +7,17 @@
 #' }
 #'
 #' @export
-plotter <- function() {}
+plotter1 <- function() {}
+
+#' @rdname plotter1
+#'
+#' @import ggplot2
+#'
+#' @examples
+#' \donttest{
+#'  ggplot(cars, aes(x = speed)) + geom_histogram(bins = 20)
+#'  ggplot(cars, aes(x = speed, y = dist)) + geom_point()
+#' }
+#'
+#' @export
+plotter2 <- function() {}
