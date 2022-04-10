9810bae4623e4b0a34d42b52a1039838c4ac9f56
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-10 17:06:54 +0200

    oops

--- R/plotter.R
+++ R/plotter.R
@@ -1,5 +1,6 @@
 #' @title Plot cars data
 #'
+#' @examples
 #' if (require("ggplot2")) {
 #'  ggplot(cars, aes(x = speed)) + geom_histogram(bins = 20)
 #'  ggplot(cars, aes(x = speed, y = dist)) + geom_point()
