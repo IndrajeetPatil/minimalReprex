e243e756883793b1e0cb2d8bae529be0f030d7fe
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-10 17:37:26 +0200

    bingo

--- R/plotter.R
+++ R/plotter.R
@@ -1,25 +1,8 @@
 #' @title Plot cars data
-#'
 #' @examples
 #' if (require("ggplot2")) {
 #'  ggplot(cars, aes(x = speed)) + geom_histogram(bins = 20)
 #'  ggplot(cars, aes(x = speed, y = dist)) + geom_point()
 #' }
-#'
 #' @export
-plotter1 <- function() {}
-
-#' @rdname plotter1
-#'
-#' @import ggplot2
-#'
-#' @examples
-#' \donttest{
-#' if (require("ggplot2")) {
-#'   ggplot(cars, aes(x = speed)) + geom_histogram(bins = 20)
-#'   ggplot(cars, aes(x = speed, y = dist)) + geom_point()
-#' }
-#' }
-#'
-#' @export
-plotter2 <- function() {}
+plotter <- function() {}
