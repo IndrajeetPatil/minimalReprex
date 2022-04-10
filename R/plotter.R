a60cddfe2569e2baa907569a93982d9b3f5d1253
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-10 15:14:57 +0200

    is that the issue?

--- R/plotter.R
+++ R/plotter.R
@@ -15,10 +15,11 @@ plotter1 <- function() {}
 #'
 #' @examples
 #' \donttest{
-#'  library(ggplot2)
+#'  if (require("ggplot2")) {
 #'
 #'  ggplot(cars, aes(x = speed)) + geom_histogram(bins = 20)
 #'  ggplot(cars, aes(x = speed, y = dist)) + geom_point()
+#' }
 #' }
 #'
 #' @export
